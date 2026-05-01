1dc3ea9106e49d3cec31f6d3469cdb6de4ef3874
Georgin Reji <georgin.reji@datasemantics.co>
2026-05-01 14:27:14 +0530

    fix(env): env variable config

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,62 +4,54 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'cinema-paradiso',
+    title: "cinema-paradiso",
     htmlAttrs: {
-      lang: 'en'
+      lang: "en",
     },
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" },
+      { name: "format-detection", content: "telephone=no" },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
     // Load global SCSS file
-    '@/assets/scss/main.scss',
+    "@/assets/scss/main.scss",
   ],
   styleResources: {
-    scss: [
-      '@/assets/scss/utils/_variables.scss'
-    ]
+    scss: ["@/assets/scss/utils/_variables.scss"],
   },
-  env: {
-    baseUrl: process.env.base_url || 'http://localhost:3000/',
-    apiKey: process.env.tmdb_api_key || null
+
+  // Runtime Config with built-in dotenv support
+  publicRuntimeConfig: {
+    baseUrl: process.env.base_url || "http://localhost:3000",
+    apiKey: process.env.tmdb_api_key,
   },
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-    '~/plugins/axios.js',
-  ],
+  plugins: ["~/plugins/axios.js"],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-  ],
+  buildModules: [],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios',
+    "@nuxtjs/axios",
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
-    // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: '/',
+    baseURL: process.env.base_url,
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-
-  }
-}
+  build: {},
+};
--- plugins/axios.js
+++ plugins/axios.js
@@ -1,19 +1,22 @@
-export default function ({ $axios }, inject) {
-    $axios.defaults.baseURL = process.env.baseUrl;
-    console.log('axios url', $axios.baseURL);
-    const errorHandler = (error) => {
-      console.log('Error ==>', error);
-    };
+/**
+ * $axios - pre-configured Axios instance from Nuxt module
+ * $config - Runtime config
+ * @returns {Object} - Axios instance
+ */
+export default function ({ $axios, $config }) {
+  const errorHandler = (error) => {
+    console.log("Error ==>", error);
+  };
 
-    const onRequestHandle = (config) => {
-      config.params = {
-        api_key: process.env.apiKey
-      };
-      console.log("request made to url", config);
-      return config;
+  const onRequestHandle = (config) => {
+    config.params = {
+      ...config.params,
+      api_key: $config.apiKey,
     };
-  
-    $axios.onRequest(onRequestHandle);
-    $axios.onError(errorHandler);
-  }
-  
+    console.log("request made to url", config);
+    return config;
+  };
+
+  $axios.onRequest(onRequestHandle);
+  $axios.onError(errorHandler);
+}
